2caa24a49f855ef2e36c9c171c943eab0373160b
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-04-18 14:08:48 +0100

    longest substring sol 3 simpler

--- longest-substring/main.go
+++ longest-substring/main.go
@@ -31,41 +31,31 @@ func main() {
 
 }
 
-// sliding window implementation
 func lengthOfLongestSubstring(s string) int {
 	n := len(s)
 	if n == 0 || n == 1 {
 		return n
 	}
 
-	startIndex, currentIndex := 0, 0
 	store := make(map[byte]int)
 	maxSubstringLen := 0
 	substringLen := 0
 
-	for startIndex != n {
-		for i := currentIndex; i < n; i++ {
-			c := s[i]
-			charIdx, ok := store[c]
-			if !ok {
-				store[c] = i
-				substringLen++
-				maxSubstringLen = max(substringLen, maxSubstringLen)
-			} else {
-				startIndex = charIdx + 1
-				currentIndex = i
-
-				store[c] = i
-				for k, val := range store {
-					if val <= charIdx {
-						delete(store, k)
-						substringLen--
-					}
+	for i := 0; i < n; i++ {
+		c := s[i]
+		charIdx, ok := store[c]
+		if !ok {
+			store[c] = i
+			substringLen++
+			maxSubstringLen = max(substringLen, maxSubstringLen)
+		} else {
+			store[c] = i
+			for k, val := range store {
+				if val <= charIdx {
+					delete(store, k)
+					substringLen--
 				}
 			}
-		}
-		if currentIndex != n-1 {
-			break
 		}
 	}
 
